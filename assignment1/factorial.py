8b0d4cdbbefae04258d16e3870ac53d672a227cb
Vishal987472 <pal7102002@gmail.com>
2024-08-09 00:23:50 +0530

    done

--- assignment1/factorial.py
+++ assignment1/factorial.py
@@ -1,16 +1,10 @@
 # Get the input from the user
 num = int(input("Enter a number: "))
 
-# Initialize the factorial variable to 1
 factorial = 1
-
-# Initialize the counter variable to 1
 i = 1
 
-# Use a while loop to calculate the factorial
 while i <= num:
     factorial *= i
     i += 1
-
-# Print the result
 print("The factorial of", num, "is", factorial)
